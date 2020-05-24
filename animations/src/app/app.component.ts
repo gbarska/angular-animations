31572bbeaca2578595c4e14488e68ba6b495aed5
Gustavo B Barska <gbarska@yahoo.com>
2020-05-23 23:14:35 -0300

    recap ngClass and ngStyle

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -6,9 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- courseGoals = [
-   { title: 'Master Angular Styling', isActiveGoal: true },
-   { title: 'Understand Angular Animations', isActiveGoal: false },
-   { title: 'Master Angular Animations', isActiveGoal: false }
- ]
+//  courseGoals = [
+//    { title: 'Master Angular Styling', isActiveGoal: true },
+//    { title: 'Understand Angular Animations', isActiveGoal: false },
+//    { title: 'Master Angular Animations', isActiveGoal: false }
+//  ]
+
+isFavorite = false;
+
 }
